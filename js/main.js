b9d9b533a658c81426811ac8aa65c18b6ec2f9c6
Piotr Majewski <mail@piotrmajewski.com>
2022-10-14 09:13:43 +0200

    Planer initial implementation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 var Planer = (function () {
+    const _urlPrefix = "https://www.rejseplanen.dk/webapp/index.html?language=da_DA#";
+    const _urlSuffix = "!timeSel|depart!start|1";
     var _now = new Date();
     var _date = new Date(_now);
+    var _from = "";
+    var _to = "";
 
     function _formatDate(aDate) {
         var month = (aDate.getMonth() + 1).toString();
@@ -18,13 +22,41 @@ var Planer = (function () {
         return aDate.toTimeString().slice(0,5);
     }
 
+    function _s() {
+        return "!S|" + encodeURIComponent(_from);
+    }
+
+    function _z() {
+        return "!Z|" + encodeURIComponent(_to);
+    }
+
+    function _d() {
+        return "!date|" + _formatDate(_date);
+    }
+
+    function _t() {
+        return "!time|" + _formatTime(_date);
+    }
+
     return {
+        from : function(from) {
+            _from = from;
+        },
+
+        to : function(to) {
+            _to = to;
+        },
+
         now : function() {
             _date = new Date(_now);
         },
 
         afterMinutes : function(minutes) {
             _date.setMinutes(_now.getMinutes() + minutes);
+        },
+
+        search : function() {
+            var uri = _urlPrefix + _s() + _z() + _d() + _t() + _urlSuffix;
         }
     };
 })();
